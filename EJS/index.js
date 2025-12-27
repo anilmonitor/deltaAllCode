617cdb699bf0fbc298bafc2ed8f0e3c6f1d1bd78
ANIL_L <anilarangi6@gmail.com>
2025-12-28 03:26:30 +0530

    Add learnEjs.ejs and learning it

--- EJS/index.js
+++ EJS/index.js
@@ -6,10 +6,17 @@ app.listen(PORT, () => {
   console.log("App is listening on port", PORT);
 });
 
+//ejs
 app.set("view engine", "ejs");
-
-
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+});
+
+app.get("/ejs", (req, res) => {
+  const username = req.query.username;
+  res.render("learnEjs.ejs", {
+    name: username,
+    age: 10,
+  });
 });
