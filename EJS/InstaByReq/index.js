531dd6c67d1b728cc4524ca969c3a4ccb7fe6c99
ANIL_L <anilarangi6@gmail.com>
2025-12-31 14:16:53 +0530

    Add instaByRq

---
+++ EJS/InstaByReq/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const finalData = require("./data.json");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.listen(PORT, () => {
+  console.log("App is listening on port", PORT);
+});
+
+//insta PAGE
+app.get("/instagram/:username", (req, res) => {
+  const { username } = req.params;
+  const data = finalData[username];
+
+  if (!data) {
+    res.render("notFound.ejs", { username });
+  }
+
+  res.render("insta.ejs", { data });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Search for: i. /instagram/cats and ii. /instagram/dogs</h1>");
+});
