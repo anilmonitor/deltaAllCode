9d752ef206083b1ace16c94b766c9a5cd9c39999
ANIL_L <anilarangi6@gmail.com>
2026-01-02 00:51:06 +0530

    new operator

--- OOPSjs/app.js
+++ OOPSjs/app.js
@@ -5,6 +5,7 @@
 //   age: 23,
 // };
 
+/*
 //factor function
 function personMaker(name, age) {
   const person = {
@@ -15,10 +16,28 @@ function personMaker(name, age) {
     },
   };
   return person;
-}
+} 
 
 let s1 = personMaker("Anil", 22);
 let s2 = personMaker("Rahul", 23);
 
 console.log(s1);
 console.log(s2);
+
+*/
+
+//new operator --> It does't return and It start with capital letter
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.talk = function () {
+  console.log(`I am ${this.name}`);
+};
+
+let p1 = new Person("ANIL", 23);
+let p2 = new Person("Rahul", 25);
+
+console.log(p1);
+console.log(p2);
