1d68cb3acc81ff363fc857879f96530f0bbf93b2
ANIL_L <anilarangi6@gmail.com>
2026-01-01 23:57:25 +0530

    Create factory function

--- OOPSjs/app.js
+++ OOPSjs/app.js
@@ -1,6 +1,18 @@
-let ar = [1, 2, 3, 4];
+// let ar = [1, 2, 3, 4];
 
-let obj = {
-  name: "Anil",
-  age: 23,
-};
+// let obj = {
+//   name: "Anil",
+//   age: 23,
+// };
+
+//factor function
+
+function student(name, age) {
+  const person = {
+    name: name,
+    age: age,
+    talk: function () {
+      console.log(`I am ${this.name}`);
+    },
+  };
+}
