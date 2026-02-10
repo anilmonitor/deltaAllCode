beca5cc58431d0b9307311ba3bdcf3f675115ca6
ANIL <anilarangi6@gmail.com>
2026-02-11 01:29:31 +0530

    Add ManyData

--- mongoose/index.js
+++ mongoose/index.js
@@ -23,6 +23,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 // const Emoployee = mongoose.model("Emoployee", userSchema);
 
+/*
 //inserting data to database
 const user1 = new User({ name: "Anil", gender: "Male", age: 22 });
 const user2 = new User({ name: "Anil", gender: "Male", age: 22 });
@@ -30,6 +31,35 @@ const user2 = new User({ name: "Anil", gender: "Male", age: 22 });
 user1.save(); // it return promise
 user2
     .save()
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+    */
+
+// inserting multiple document at the same time (NOT much use)
+
+User.insertMany([
+    {
+        name: "Neha Kumari",
+        gender: "Female",
+        age: 20,
+    },
+    { name: "Ritu Kumari", gender: "Female", age: 19 },
+    { name: "Rahul Kumar", gender: "Male", age: 23 },
+    {
+        name: "Anil Kumar",
+        gender: "Male",
+        age: 22,
+    },
+
+    { name: "Suman Kumari", gender: "Female", age: 22 },
+    { name: "Nitish Kumar", gender: "Male", age: 23 },
+])
+
     .then((res) => {
         console.log(res);
     })
